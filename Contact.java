204756dbe689f766341e944ca107a92d95f5a48f
Wasiq Barat <wasiqlilium1399@gmail.com>
2022-03-04 17:09:30 +0330

    feat: We created Contact

---
+++ Contact.java
@@ -0,0 +1,65 @@
+public class Contact {
+    private String group;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private PhoneNumber phoneNumber;
+    private Address address;
+
+    public Contact() {
+        this.group = "";
+        this.email = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.phoneNumber = new PhoneNumber();
+        this.address = new Address();
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(PhoneNumber phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
+    }
+}
